af30076ba7202ea0692f62b4bb6471935facc92c
Kevin Manning <kkevin13@byu.edu>
2026-09-17 08:48:05 -0600

    Fixing main.go

--- main.go
+++ main.go
@@ -1,10 +1,8 @@
 package main
-
 import (
-	"fmt"
+	"log"
 	"net/http"
 )
-
 func main() {
 	mux := http.NewServeMux()
 
@@ -14,5 +12,5 @@ func main() {
 	log.Print("starting server on 4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
-	})
+}
 
