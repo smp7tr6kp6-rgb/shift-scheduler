e60e5e0fc54a8f4ba00489f9e28d21392684f4f6
Kevin Manning <kkevin13@byu.edu>
2026-09-17 08:45:51 -0600

    Add server error message and server running log.

--- main.go
+++ main.go
@@ -11,5 +11,8 @@ func main() {
 	fileServer := http.FileServer(http.Dir("./static"))
 	mux.Handle("GET /static/", http.StripPrefix("/static/", fileServer))
 
+	log.Print("starting server on 4000")
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
 	})
 
